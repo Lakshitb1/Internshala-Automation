4b13a4bb537618b3fa12dc2abe8873bc85994dfc
Lakshitb1 <101925867+Lakshitb1@users.noreply.github.com>
2024-08-13 16:39:18 +0530

    Add files via upload

---
+++ data.js
@@ -0,0 +1,15 @@
+module.exports = {
+    "cover" : `With a strong foundation in both front-end and back-end development, I am confident in my ability to contribute effectively to your team. My technical expertise includes:
+    Flutter: Proficient in developing cross-platform mobile applications, ensuring a seamless and responsive user experience.
+    Node.js & Express.js: Experienced in building robust server-side applications and RESTful APIs, ensuring efficient and scalable back-end solutions.
+    MongoDB: Skilled in utilizing NoSQL databases for flexible and high-performance data storage.
+    Programming Languages:
+    C++: Competent in object-oriented programming and algorithm development.
+    JavaScript: Proficient in both front-end and back-end development, leveraging frameworks and libraries to create dynamic and interactive web applications.
+    Python: Adept at scripting, automation, and developing comprehensive applications.
+    These technical skills, combined with my problem-solving abilities and dedication to continuous learning, make me a strong candidate for this role.`,
+
+    "College": `Thapar University`,
+    "Degeree": `B.E`,
+    "Stream": 'Computer Engineering',
+}
